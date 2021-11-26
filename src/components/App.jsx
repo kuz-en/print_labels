33821c9bb07ab1488b19c2dd76dc170d76599125
kuz-en <bio19@ya.ru>
2021-11-26 20:22:50 +0300

    completed filter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -6,11 +6,12 @@ import labels from '../data/labels';
 
 function App() {
     const [printLabels, setPrintLabels] = useState(labels);
+    const [basket, setBasket] = useState(labels);
     const [listForPrint, setListForPrint] = useState([]);
 
     function createLabelsForPrint() {
         let prepareForPrint = [];
-        const labelsForPrint = printLabels.filter((item) => item.qty > 0);
+        const labelsForPrint = basket.filter((item) => item.qty > 0);
         labelsForPrint.forEach((item) => {
             for (let i = 0; i < item.qty; i++) {
                 prepareForPrint = [...prepareForPrint, Object.assign({}, item)];
@@ -23,32 +24,92 @@ function App() {
     function handleInput(value, itemId) {
         const order = printLabels.find((item) => item.id === itemId);
         order.qty = +value;
-        setPrintLabels([...printLabels]);
+        setBasket([...printLabels]);
         createLabelsForPrint();
     }
 
     function plusQty(itemId) {
-        const order = printLabels.find((item) => item.id === itemId);
+        const order = basket.find((item) => item.id === itemId);
         order.qty = order.qty + 1;
-        setPrintLabels([...printLabels]);
+        // setBasket([...printLabels]);
         createLabelsForPrint();
     }
 
     function minusQty(itemId) {
         const order = printLabels.find((item) => item.id === itemId);
         order.qty = order.qty - 1;
-        setPrintLabels([...printLabels]);
+        setBasket([...printLabels]);
         createLabelsForPrint();
     }
 
     function clearInputsAfterPrint() {
         printLabels.find((item) => (item.qty = 0));
+        basket.find((item) => (item.qty = 0));
         setListForPrint([]);
+    }
+
+    function filterLabels(value) {
+        const result = labels.filter((item) => item.category === value);
+        result.length > 0 ? setPrintLabels(result) : setPrintLabels(labels);
     }
 
     return (
         <div className='container' style={{ marginTop: '40px' }}>
             <div className='row'>
+                <form>
+                    <div className='form-check'>
+                        <label>
+                            <input
+                                type='radio'
+                                name='react-tips'
+                                value=''
+                                onChange={(e) => {
+                                    filterLabels(e.target.value);
+                                }}
+                                //checked={}
+                                className='form-check-input'
+                            />
+                            Все
+                        </label>
+                    </div>
+                    <div className='form-check'>
+                        <label>
+                            <input
+                                type='radio'
+                                name='react-tips'
+                                value='diapers'
+                                onChange={(e) => {
+                                    filterLabels(e.target.value);
+                                }}
+                                //checked={}
+                                className='form-check-input'
+                            />
+                            Option 1
+                        </label>
+                    </div>
+
+                    <div className='form-check'>
+                        <label>
+                            <input
+                                type='radio'
+                                name='react-tips'
+                                value='pants'
+                                onChange={(e) => {
+                                    filterLabels(e.target.value);
+                                }}
+                                //checked={type === 'diapers'}
+                                className='form-check-input'
+                            />
+                            Option 2
+                        </label>
+                    </div>
+
+                    <div className='form-group'>
+                        <button className='btn btn-primary mt-2' type='submit'>
+                            Save
+                        </button>
+                    </div>
+                </form>
                 <table className='u-full-width'>
                     <thead>
                         <tr>
@@ -59,7 +120,7 @@ function App() {
                         </tr>
                     </thead>
                     <tbody>
-                        {labels.map((item) => {
+                        {printLabels.map((item) => {
                             return (
                                 <Label
                                     id={item.id}
--- src/data/labels.jsx
+++ src/data/labels.jsx
@@ -4,48 +4,56 @@ const labels = [
         text: `^XA ^CF0,80 ^FO90,120^FDGOON^FS ^XZ`,
         id: 1,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Huggies',
         text: `^XA ^CF0,80 ^FO90,120^FDHUGGIES^FS ^XZ`,
         id: 2,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Joonies',
         text: `^XA ^CF0,80 ^FO90,120^FDJOONIES^FS ^XZ`,
         id: 3,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Lovular',
         text: `^XA ^CF0,80 ^FO90,120^FDLOVULAR^FS ^XZ`,
         id: 4,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Merries',
         text: `^XA ^CF0,80 ^FO90,120^FDMERRIES^FS ^XZ`,
         id: 5,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Moony',
         text: `^XA ^CF0,80 ^FO90,120^FDMOONY^FS ^XZ`,
         id: 6,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Pampers',
         text: `^XA ^CF0,80 ^FO90,120^FDPAMPERS^FS ^XZ`,
         id: 7,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Yokosun',
         text: `^XA ^CF0,80 ^FO90,120^FDYOKOSUN^FS ^XZ`,
         id: 8,
         qty: 0,
+        category: 'diapers',
     },
     {
         title: 'Штанишки для фиксации прокладок, размер M',
@@ -62,6 +70,7 @@ const labels = [
         ^XZ`,
         id: 9,
         qty: 0,
+        category: 'pants',
     },
     {
         title: 'Штанишки для фиксации прокладок, размер L',
@@ -78,6 +87,7 @@ const labels = [
         ^XZ`,
         id: 10,
         qty: 0,
+        category: 'pants',
     },
     {
         title: 'Штанишки для фиксации прокладок, размер XL',
@@ -94,6 +104,7 @@ const labels = [
         ^XZ`,
         id: 11,
         qty: 0,
+        category: 'pants',
     },
 ];
 
